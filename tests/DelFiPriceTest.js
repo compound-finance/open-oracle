bb75d5d3008eb15adfca76b5b07d95834d47cf90
Jared Flatow <jflatow@gmail.com>
2019-06-13 10:15:54 -0700

    Add a little more test coverage and tighter gas assertions

--- tests/DelFiPriceTest.js
+++ tests/DelFiPriceTest.js
@@ -17,7 +17,13 @@ describe('Oracle', () => {
       '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf12',
       '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf13',
       '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf14',
+    ].map(web3.eth.accounts.privateKeyToAccount);
+    const nonSources = [
       '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf15',
+      '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf16',
+      '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf17',
+      '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf18',
+      '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf19'
     ].map(web3.eth.accounts.privateKeyToAccount);
     const oracle = await deploy('Oracle', [], {from: account});
     const delfi = await deploy('DelFiPrice', [oracle.address, sources.map(a => a.address)]);
@@ -26,15 +32,15 @@ describe('Oracle', () => {
     // Reads a price of an asset that doesn't exist yet
     expect(await delfi.methods.prices('ETH').call()).numEquals(0);
 
-    async function postPrices(timestamp, priceses, symbols) {
+    async function postPrices(timestamp, priceses, symbols, signers = sources) {
       const messages = [], signatures = [];
       priceses.forEach((prices, i) => {
         let {
           message,
           signature,
           signatory
-        } = sign(encode(timestamp, prices.map(([symbol, price]) => [bytes(symbol), uint256(price)])), sources[i].privateKey);
-        expect(signatory).toEqual(sources[i].address);
+        } = sign(encode(timestamp, prices.map(([symbol, price]) => [bytes(symbol), uint256(price)])), signers[i].privateKey);
+        expect(signatory).toEqual(signers[i].address);
         messages.push(message);
         signatures.push(signature);
       })
@@ -83,7 +89,7 @@ describe('Oracle', () => {
 
     /** Posts a price for 2 symbols from 3 sources, stores median **/
 
-    const post3 = await postPrices(now + 2, [
+    const post3a = await postPrices(now + 2, [
       [
         ['BTC', 9000],
         ['ETH', 257]
@@ -97,15 +103,53 @@ describe('Oracle', () => {
         ['ETH', 255]
       ]
     ], ['BTC', 'ETH']);
-    expect(post3.gasUsed).toBeLessThan(600000);
+    expect(post3a.gasUsed).toBeLessThan(500000);
 
     expect(await getPrice('BTC')).numEquals(8500);
     expect(await getPrice('ETH')).numEquals(256);
 
 
-    /** Posts a price from a non-source, median does not change **/
+    /** Posts again with fresher timestamp, gas should still be cheaper **/
 
-    // XXX
+    const post3b = await postPrices(now + 3, [
+      [
+        ['BTC', 9000],
+        ['ETH', 257]
+      ],
+      [
+        ['BTC', 8500],
+        ['ETH', 256]
+      ],
+      [
+        ['BTC', 8000],
+        ['ETH', 255]
+      ]
+    ], ['BTC', 'ETH']);
+    expect(post3b.gasUsed).toBeLessThan(post3a.gasUsed * 0.8);
+
+    expect(await getPrice('BTC')).numEquals(8500);
+    expect(await getPrice('ETH')).numEquals(256);
+
+
+    /** Posts a price from non-sources, median does not change **/
+
+    const postNon3 = await postPrices(now + 3, [
+      [
+        ['BTC', 19000],
+        ['ETH', 1257]
+      ],
+      [
+        ['BTC', 18500],
+        ['ETH', 1256]
+      ],
+      [
+        ['BTC', 18000],
+        ['ETH', 1255]
+      ]
+    ], ['BTC', 'ETH'], nonSources);
+
+    expect(await getPrice('BTC')).numEquals(8500);
+    expect(await getPrice('ETH')).numEquals(256);
 
   }, 30000);
 });
