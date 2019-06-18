db202158a66dffe9542a9d2c071fbfd485b9b793
Coburn Berry <coburn@compound.finance>
2019-06-17 17:29:33 -0700

    encode function call

--- poster/src/index.ts
+++ poster/src/index.ts
@@ -1,13 +1,69 @@
 import {run} from './postWithRetries';
+import AbiCoder from 'web3-eth-abi';
 
-function () {
+function main () {
   // read all this from input args or env
   // write tests
-  // const abi = get abi from env
-  // const address = get address from env
-  // const method 
-  // const payload 
-  // const trxInfo
-  const oracleView : OpenOracleView = new web3.eth.Contract(abi, address);
-  run(oracleView.methods[method](payload), trxInfo);
+  const address = loadViewAddress();
+  // construct abi from that method name
+  // const payload = loadPayload();
+  // const trxInfo = buildTrxData();
+  // const oracleView : OpenOracleView = new web3.eth.Contract(abi, address);
+  // run(oracleView.methods[method](payload), trxInfo);
+}
+
+function loadViewAddress(env?: {[key: string]: string}) {
+  return getEnvVar("view-address", env)
+}
+
+function loadPayload() {
+  // let sources = getEnvVar("sources", env);
+  // fetch json from sources
+}
+
+function buildTrxData(params : OpenOraclePayload[], env?: {[key: string]: string}) {
+  const functionName = getEnvVar("view-function-name", env);
+  const types = findTypes(functionName);
+
+  let messages = params.map(x => x.message);
+  let signatures = params.map(x => x.signature);
+
+  // see https://github.com/ethereum/web3.js/blob/2.x/packages/web3-eth-abi/src/AbiCoder.js#L112
+  return AbiCoder.encodeFunctionSignature(functionName) +
+    AbiCoder
+    .encodeParameters(types, [messages, signatures])
+    .replace('0x', '');
+}
+
+function getEnvVar(name: string, env?: {[key: string]: string}): string {
+  const theEnv = env !== undefined ? env : process.env;
+
+  const result: string | undefined = theEnv[name];
+
+  if (result) {
+    return result;
+  } else {
+    throw `Missing required env var: ${name}`;
+  }
+}
+
+function findTypes(functionName : string) : string[] {
+  let start = functionName.indexOf("(") + 1;
+  let types = functionName
+    .slice(start, functionName.lastIndexOf(")"))
+    .split(",");
+  return types
+}
+
+// encodeFunctionCall(jsonInterface, params) {
+//   return (
+//     this.encodeFunctionSignature(jsonInterface) +
+//       this.encodeParameters(jsonInterface.inputs, params).replace('0x', '')
+//   );
+// }
+
+export {
+  buildTrxData,
+  findTypes,
+  loadViewAddress
 }
--- poster/tests/poster_test.ts
+++ poster/tests/poster_test.ts
@@ -1,6 +1,53 @@
-import {run} from '../src/poster';
+import {buildTrxData, findTypes, loadViewAddress} from '../src/index';
+import AbiCoder from 'web3-eth-abi';
 
-test('Poster', async () => {
-  // TODO: Add test cases
-  expect(await run()).toEqual(22);
-});
+describe('loading poster arguments from environment', () => {
+  test.only('loadViewAddress', () => {
+    let viewAddress = loadViewAddress({"view-address": "0xMYVIEW"});
+    expect(viewAddress).toEqual("0xMYVIEW");
+  });
+
+  test('loadPayload', () => {
+    // hits the http endpoints, encodes a transaction
+  });
+
+  test.only('buildTrxData', () => {
+    let encodedMessage = '0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf10'
+
+    let signedMessage = '0x04a78a7b3013f6939da19eac6fd1ad5c5a20c41bcc5d828557442aad6f07598d029ae684620bec13e13d018cba0da5096626e83cfd4d5356d808d7437a0a5076000000000000000000000000000000000000000000000000000000000000001c'
+
+    let data = buildTrxData(
+      [{message: encodedMessage, signature: signedMessage}],
+      {"view-function-name": "writePrices(bytes[],bytes[])"}
+    );
+
+    let assumedAbi = {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "anything",
+          "type": "bytes[]"
+        },
+        {
+          "name": "whatever",
+          "type": "bytes[]"
+        }
+      ],
+      "name": "writePrices",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    };
+
+    let officialWeb3Encoding =
+      AbiCoder.encodeFunctionCall(assumedAbi, [[encodedMessage], [signedMessage]]);
+
+    expect(data).toEqual(officialWeb3Encoding);
+  });
+
+  test.only('findTypes', () => {
+    let typeString =  "writePrices(bytes[],bytes[])"
+    expect(findTypes(typeString)).toEqual(["bytes[]", "bytes[]"])
+  })
+})
