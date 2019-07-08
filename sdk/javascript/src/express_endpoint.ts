018620ad716d43c077167e4e418cc70e91585a95
Jared Flatow <jflatow@gmail.com>
2019-07-08 11:47:43 -0700

    Add a symbols filter to the endpoint

--- sdk/javascript/src/express_endpoint.ts
+++ sdk/javascript/src/express_endpoint.ts
@@ -1,24 +1,56 @@
 import express from 'express';
 import {encode, sign} from './reporter';
 
+type UnixT = number;
+type Pairs = object | [any, any][];
+type Query = any;
+
+function flatStringList(param: string | string[]): string[] {
+  if (Array.isArray(param))
+    return param.reduce((a: string[], s) => a.concat(flatStringList(s)), []);
+  return param.split(',');
+}
+
+function upper(str: string): string {
+  return str.toUpperCase();
+}
+
+export const Filters = {
+  symbols: (pairs: Pairs, query: Query): Pairs => {
+    if (query.symbols) {
+      const symbols = new Set(flatStringList(query.symbols).map(upper));
+      if (Array.isArray(pairs))
+        return pairs.filter(([k, _]) => symbols.has(upper(k)));
+      return Object.entries(pairs).reduce((a, [k, v]) => {
+        if (symbols.has(upper(k)))
+          a[k] = v;
+        return a;
+      }, {});
+    }
+    return pairs;
+  }
+}
+
 export function endpoint(
   privateKey: string,
-  getter: (number) => Promise<[number, object]>,
+  getter: (now: UnixT) => Promise<[UnixT, Pairs]>,
+  filter: (pairs: Pairs, query: Query) => Pairs = Filters.symbols,
   kind: string = 'prices',
   path: string = `/${kind}.json`
 ): express.Application {
   return express()
     .get(path, async (req, res) => {
       const [timestamp, pairs] = await getter(Math.floor(+new Date / 1000));
+      const filtered = filter(pairs, req.query);
       const {
         message,
         signature
-      } = sign(encode(kind, timestamp, pairs), privateKey);
+      } = sign(encode(kind, timestamp, filtered), privateKey);
       res.json({
         message,
         signature,
         timestamp: timestamp,
-        [kind]: pairs
+        [kind]: filtered
       });
     });
 }
