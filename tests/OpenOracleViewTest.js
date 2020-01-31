601f54df980f3247a689c5a6260c8958b2768cde
maxwolff <maxcwolff@gmail.com>
2020-01-31 09:46:50 -0800

    fix open oracle revert test

--- tests/OpenOracleViewTest.js
+++ tests/OpenOracleViewTest.js
@@ -2,6 +2,6 @@
 describe('OpenOracleView', () => {
   it('view with no sources is invalid', async () => {
     const oracleData = await saddle.deploy('OpenOracleData', []);
-	await expect(saddle.deploy('OpenOracleView', [oracleData._address, []])).rejects.toRevert('revert "Must initialize with sources');
+	await expect(saddle.deploy('OpenOracleView', [oracleData._address, []])).rejects.toRevert('revert Must initialize with sources');
   });
 });
