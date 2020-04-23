25e150fefbf2efc29b659992b3a7db250edae101
Antonina Norair <antonina.norair@gmail.com>
2020-04-23 11:08:22 -0700

    Revert some of the changes back to master delfi

--- tests/IntegrationTest.js
+++ tests/IntegrationTest.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const Web3 = require('web3');
 const compose = require('docker-compose');
+const contract = require('eth-saddle/dist/contract');
 const { exec } = require('child_process');
 const util = require('util');
 const DockerProvider = require('./DockerProvider');
-const contract = require('eth-saddle/dist/contract');
 
 const execute = util.promisify(exec);
 
@@ -34,7 +34,7 @@ async function waitForLogs(serviceLogPairs) {
   if (complete) {
     return;
   } else {
-    await sleep(10000);
+    await sleep(5000);
     await waitForLogs(serviceLogPairs);
   }
 }
@@ -61,8 +61,8 @@ describe('Integration', () => {
       const delfi = await contract.getContractAt(web3, 'DelFiPrice', {build_dir: '.dockerbuild_cp', trace: false, extra_build_files: []}, '0x5b1869D9A4C187F2EAa108f3062412ecf0526b24');
 
       expect(await delfi.methods.prices('BTC').call({from: accounts[0]})).numEquals(0);
-      expect(await delfi.methods.prices('ETH').call({from: accounts[0]})).numEquals(0);
-      expect(await delfi.methods.prices('ZRX').call({from: accounts[0]})).numEquals(0);
+      expect(await delfi.methods.prices('ETH').call({from: accounts[0]})).numEquals('260000000');
+      expect(await delfi.methods.prices('ZRX').call({from: accounts[0]})).numEquals('580000');
     } finally {
       await compose.down({cwd: root});
     }
