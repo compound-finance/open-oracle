5b40ae9c011b3c932c76b379495c6574c1e73235
Antonina Norair <antonina.norair@gmail.com>
2020-04-23 10:54:29 -0700

    Fix compilation error

--- tests/contracts/ProxyPriceOracle.sol
+++ tests/contracts/ProxyPriceOracle.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.6.6;
-import "../../contracts/DelFiPrice.sol";
+import "../../contracts/AnchoredPriceView.sol";
 
 
 // @dev mock version of price oracle proxy, allowing manually setting return values
