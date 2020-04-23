7195518a55332e469e25361f36ae020709219be8
coburncoburn <coburn@compound.finance>
2020-04-22 20:03:52 -0700

    create new view instead of changing delfi

---
+++ contracts/AnchoredPriceView.sol
@@ -0,0 +1,302 @@
+pragma solidity ^0.6.6;
+pragma experimental ABIEncoderV2;
+
+import "./OpenOraclePriceData.sol";
+
+interface AnchorPriceOracle {
+     function getUnderlyingPrice(address) external view returns (uint256);
+}
+
+/**
+ * @notice Price feed conforming to Price Oracle Proxy interface and
+ * using a single open oracle reporter, anchored to and falling back to
+ * the Compound v2 oracle system.
+ * @author Compound Labs, Inc.
+ */
+contract AnchoredPriceView {
+    /// @notice standard amount for the Dollar
+    uint constant oneDollar = 1e6;
+
+    /// @notice The event emitted when the median price is updated
+    event PriceUpdated(string symbol, uint64 price);
+
+    /// @notice The event emitted when new prices are posted but the median price is not updated due to the anchor
+    event PriceGuarded(string symbol, uint64 median, uint64 anchor);
+
+    /// @notice The CToken contracts addresses
+    struct CTokens {
+        address cEthAddress;
+        address cUsdcAddress;
+        address cDaiAddress;
+        address cRepAddress;
+        address cWbtcAddress;
+        address cBatAddress;
+        address cZrxAddress;
+        address cSaiAddress;
+        address cUsdtAddress;
+    }
+
+    /// @notice The reporter address whose prices checked against the median for safety
+    AnchorPriceOracle immutable anchor;
+
+    /// @notice The highest ratio of the new median price to the anchor price that will still trigger the median price to be updated
+    uint256 upperBoundAnchorRatio;
+
+    /// @notice The lowest ratio of the new median price to the anchor price that will still trigger the median price to be updated
+    uint256 lowerBoundAnchorRatio;
+
+    /// @notice The mapping of medianized prices per symbol
+    mapping(string => uint64) public prices;
+
+    /// @notice The binary representation for 'ETH' symbol , used for string comparison
+    bytes32 constant symbolEth = keccak256(abi.encodePacked("ETH"));
+
+    /// @notice The binary representation for 'USDC' symbol, used for string comparison
+    bytes32 constant symbolUsdc = keccak256(abi.encodePacked("USDC"));
+
+    /// @notice The binary representation for 'DAI' symbol, used for string comparison
+    bytes32 constant symbolDai = keccak256(abi.encodePacked("DAI"));
+
+    /// @notice The binary representation for 'REP' symbol, used for string comparison
+    bytes32 constant symbolRep = keccak256(abi.encodePacked("REP"));
+
+    /// @notice The binary representation for 'BTC' symbol, used for string comparison
+    bytes32 constant symbolWbtc = keccak256(abi.encodePacked("BTC"));
+
+    /// @notice The binary representation for 'BAT' symbol, used for string comparison
+    bytes32 constant symbolBat = keccak256(abi.encodePacked("BAT"));
+
+    /// @notice The binary representation for 'ZRX' symbol, used for string comparison
+    bytes32 constant symbolZrx = keccak256(abi.encodePacked("ZRX"));
+
+    /// @notice The binary representation for 'SAI' symbol, used for string comparison
+    bytes32 constant symbolSai = keccak256(abi.encodePacked("SAI"));
+
+    /// @notice The binary representation for 'SAI' symbol, used for string comparison
+    bytes32 constant symbolUsdt = keccak256(abi.encodePacked("USDT"));
+
+    /// @notice Address of the cEther contract
+    address public immutable cEthAddress;
+
+    /// @notice Address of the cUSDC contract
+    address public immutable cUsdcAddress;
+
+    /// @notice Address of the cDAI contract
+    address public immutable cDaiAddress;
+
+    /// @notice Address of the cREP contract
+    address public immutable cRepAddress;
+
+    /// @notice Address of the cWBTC contract
+    address public immutable cWbtcAddress;
+
+    /// @notice Address of the cBAT contract
+    address public immutable cBatAddress;
+
+    /// @notice Address of the cZRX contract
+    address public immutable cZrxAddress;
+
+    /// @notice Address of the cSAI contract
+    address public immutable cSaiAddress;
+
+    /// @notice Address of the cUsdt contract
+    address public immutable cUsdtAddress;
+
+    /// @notice the Open Oracle Reporter price source
+    address public immutable source;
+
+    /// @notice the Open Oracle Price Data contract
+    OpenOraclePriceData public immutable priceData;
+
+    /**
+     * @param data_ Address of the Oracle Data contract
+     * @param source_ The reporter address whose price will be used if it matches the anchor
+     * @param anchor_ The PriceOracleProxy that will be used to verify source price, or serve prices not given by the source
+     * @param anchorToleranceMantissa_ The tolerance allowed between the anchor and median. A tolerance of 10e16 means a new median that is 10% off from the anchor will still be saved
+     * @param tokens_ The CTokens struct that contains addresses for CToken contracts
+     */
+    constructor(OpenOraclePriceData data_,
+                address source_,
+                address anchor_,
+                uint anchorToleranceMantissa_,
+                CTokens memory tokens_) public {
+        source = source_;
+        anchor = AnchorPriceOracle(anchor_);
+        priceData = data_;
+
+        require(anchorToleranceMantissa_ < 100e16, "Anchor Tolerance is too high");
+        upperBoundAnchorRatio = 100e16 + anchorToleranceMantissa_;
+        lowerBoundAnchorRatio = 100e16 - anchorToleranceMantissa_;
+
+        cEthAddress = tokens_.cEthAddress;
+        cUsdcAddress = tokens_.cUsdcAddress;
+        cDaiAddress = tokens_.cDaiAddress;
+        cRepAddress = tokens_.cRepAddress;
+        cWbtcAddress = tokens_.cWbtcAddress;
+        cBatAddress = tokens_.cBatAddress;
+        cZrxAddress = tokens_.cZrxAddress;
+        cSaiAddress = tokens_.cSaiAddress;
+        cUsdtAddress = tokens_.cUsdtAddress;
+    }
+
+    /**
+     * @notice Primary entry point to post and recalculate prices
+     * @dev We let anyone pay to post anything, but only prices from configured source will be stored in the view
+     * @param messages The messages to post to the oracle
+     * @param signatures The signatures for the corresponding messages
+     */
+    function postPrices(bytes[] calldata messages, bytes[] calldata signatures, string[] calldata symbols) external {
+        require(messages.length == signatures.length, "messages and signatures must be 1:1");
+
+        // Save the prices
+        for (uint i = 0; i < messages.length; i++) {
+            priceData.put(messages[i], signatures[i]);
+        }
+
+        // load usdc for using in loop to convert anchor prices to dollars
+        uint256 usdcPrice = anchor.getUnderlyingPrice(cUsdcAddress);
+
+        // Try to update the view storage
+        for (uint i = 0; i < symbols.length; i++) {
+            string memory symbol = symbols[i];
+            address tokenAddress = getCTokenAddress(symbol);
+            uint64 sourcePrice = priceData.getPrice(source, symbol);
+            uint64 anchorPrice = getAnchorPrice(tokenAddress, usdcPrice);
+
+            if (anchorPrice == 0 || tokenAddress == cUsdcAddress || tokenAddress == cUsdtAddress) {
+                emit PriceGuarded(symbol, sourcePrice, anchorPrice);
+            } else {
+                uint256 anchorRatioMantissa = uint256(sourcePrice) * 100e16 / anchorPrice;
+                // Only update the view's price if the source price is within a bound, and it is a new median
+                if (anchorRatioMantissa <= upperBoundAnchorRatio && anchorRatioMantissa >= lowerBoundAnchorRatio) {
+                    // only update and emit event if the source price is new, otherwise do nothing
+                    if (prices[symbol] != sourcePrice) {
+                        prices[symbol] = sourcePrice;
+                        emit PriceUpdated(symbol, sourcePrice);
+                    }
+                } else {
+                    emit PriceGuarded(symbol, sourcePrice, anchorPrice);
+                }
+            }
+        }
+    }
+
+    /**
+     * @notice Flags that this contract is meant to be compatible with Compound v2 PriceOracle interface.
+     * @return true, this contract is meant to be used as Compound v2 PriceOracle interface.
+     */
+    function isPriceOracle() external pure returns (bool) {
+        return true;
+    }
+
+    /**
+     * @dev fetch price in eth from proxy and convert to usd price using anchor usdc price.
+     * @dev Anchor usdc price has 30 decimals, and anchor general price has 18 decimals, so multiplying by 1e18 and by 1e30 yields 1e6
+     */
+    function getAnchorPrice(address tokenAddress, uint256 usdcPrice) public view returns (uint64) {
+
+        if ( tokenAddress == cUsdcAddress || tokenAddress == cUsdtAddress )  {
+            // hard code to 1 dollar
+            return uint64(oneDollar);
+        }
+
+        uint priceInEth = anchor.getUnderlyingPrice(tokenAddress);
+        uint additionalScale;
+        if ( tokenAddress == cWbtcAddress ){
+            // wbtc proxy price is scaled 1e(36 - 8) = 1e28, so we need 8 more to get to 36
+            additionalScale = 1e8;
+        } else {
+            // all other tokens are scaled 1e18, so we need 18 more to get to 36
+            additionalScale = 1e18;
+        }
+
+        // usdcPrice has 30 decimals, so final result has 6
+        return uint64(mul(priceInEth, additionalScale) / usdcPrice);
+    }
+
+    /**
+     * @notice Implements the method of the PriceOracle interface of Compound v2.
+     * @dev converts from 1e6 decimals of Open Oracle to 1e(36 - underlyingDecimals) of PriceOracleProxy
+     * @param cToken The cToken address for price retrieval
+     * @return The price for the given cToken address
+     */
+    function getUnderlyingPrice(address cToken) external view returns (uint256) {
+        uint256 priceSixDecimals;
+
+        if(cToken == cUsdcAddress || cToken == cUsdtAddress) {
+            priceSixDecimals = oneDollar;
+        } else if (cToken == cWbtcAddress || cToken == cEthAddress) {
+            priceSixDecimals = prices[getOracleKey(cToken)];
+        } else {
+            uint256 usdPerEth = prices["ETH"];
+            uint256 ethPerToken = anchor.getUnderlyingPrice(cToken);
+            // usdPerEth - 6 decimals
+            // ethPerStandardErc20 - 18 decimals
+            // divide by 1e18 to drop down to 6 decimals
+            priceSixDecimals = mul(usdPerEth, ethPerToken) / 1e18;
+        }
+
+        // comptroller expects price to have 18 decimals,
+        // and additionally upscaled by 1e18 - underlyingdecimals
+        // base decimals is 1e6, so start by addint twelve
+        uint256 additionalScale;
+        if ( cToken == cUsdcAddress || cToken == cUsdtAddress )  {
+            additionalScale = 1e24;
+        } else if ( cToken == cWbtcAddress )  {
+            additionalScale = 1e22;
+        } else {
+            additionalScale = 1e12;
+        }
+
+        return mul(priceSixDecimals, additionalScale);
+    }
+
+    /**
+     * @notice Returns the cToken address for symbol
+     * @param symbol The symbol to map to cToken address
+     * @return The cToken address for the given symbol
+     */
+    function getCTokenAddress(string memory symbol) public view returns (address) {
+        bytes32 symbolHash = keccak256(abi.encodePacked(symbol));
+        if (symbolHash == symbolEth) return cEthAddress;
+        if (symbolHash == symbolUsdc) return cUsdcAddress;
+        if (symbolHash == symbolDai) return cDaiAddress;
+        if (symbolHash == symbolRep) return cRepAddress;
+        if (symbolHash == symbolWbtc) return cWbtcAddress;
+        if (symbolHash == symbolBat) return cBatAddress;
+        if (symbolHash == symbolZrx) return cZrxAddress;
+        if (symbolHash == symbolSai) return cSaiAddress;
+        if (symbolHash == symbolUsdt) return cUsdtAddress;
+        revert("Unknown token symbol");
+    }
+
+    /**
+     * @notice Returns the symbol for cToken address
+     * @param cToken The cToken address to map to symbol
+     * @return The symbol for the given cToken address
+     */
+    function getOracleKey(address cToken) public view returns (string memory) {
+        if (cToken == cEthAddress) return "ETH";
+        if (cToken == cUsdcAddress) return "USDC";
+        if (cToken == cDaiAddress) return "DAI";
+        if (cToken == cRepAddress) return "REP";
+        if (cToken == cWbtcAddress) return "BTC";
+        if (cToken == cBatAddress) return "BAT";
+        if (cToken == cZrxAddress) return "ZRX";
+        if (cToken == cSaiAddress) return "SAI";
+        if (cToken == cUsdtAddress) return "USDT";
+        revert("Unknown token address");
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+}
---
+++ tests/AnchoredPriceViewTest.js
@@ -0,0 +1,436 @@
+const { encode, sign } = require('../sdk/javascript/.tsbuilt/reporter');
+const { time, numToBigNum, numToHex, address } = require('./Helpers');
+
+async function setup() {
+  const source = web3.eth.accounts.privateKeyToAccount('0x177ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf10');
+
+  // CToken contracts addresses
+  const ctokens = {
+    cEthAddress: address(1),
+    cUsdcAddress: address(2),
+    cDaiAddress: address(3),
+    cRepAddress: address(4),
+    cWbtcAddress: address(5),
+    cBatAddress: address(6),
+    cZrxAddress: address(7),
+    cSaiAddress: address(8),
+    cUsdtAddress: address(9)
+  };
+
+  const anchorMantissa = numToHex(1e17); //1e17 equates to 10% tolerance for median to be above or below anchor
+  const priceData = await deploy('OpenOraclePriceData', []);
+  const proxyPriceOracle = await deploy('ProxyPriceOracle');
+  const anchor = proxyPriceOracle._address;
+  const delfi = await deploy('AnchoredPriceView', [
+    priceData._address,
+    source.address,
+    anchor,
+    anchorMantissa,
+    {cEthAddress: ctokens.cEthAddress,
+     cUsdcAddress: ctokens.cUsdcAddress,
+     cDaiAddress: ctokens.cDaiAddress,
+     cRepAddress: ctokens.cRepAddress,
+     cWbtcAddress: ctokens.cWbtcAddress,
+     cBatAddress: ctokens.cBatAddress,
+     cZrxAddress: ctokens.cZrxAddress,
+     cSaiAddress: ctokens.cSaiAddress,
+     cUsdtAddress: ctokens.cUsdtAddress}
+  ]);
+
+  async function postPrices(timestamp, prices2dArr, symbols, signer) {
+    const messages = [],
+      signatures = [];
+    prices2dArr.forEach((prices, i) => {
+      const signed = sign(
+        encode(
+          'prices',
+          timestamp,
+          prices.map(([symbol, price]) => [symbol, price])
+        ),
+        signer.privateKey
+      );
+      for (let { message, signature } of signed) {
+        messages.push(message);
+        signatures.push(signature);
+      }
+    });
+    return send(delfi, 'postPrices', [messages, signatures, symbols], {
+      gas: 6000000
+    });
+  }
+
+  async function getPrice(symbol) {
+    return call(delfi, 'prices', [symbol]);
+  }
+
+  async function primeAnchor() {
+    // sets up anchor for $500 eth and 10k btc
+    const usdRatio = "2000000000000000000000000000";
+
+    await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cUsdcAddress, numToHex(usdRatio)], {
+      gas: 43000
+    });
+
+    await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cEthAddress, numToHex( "1000000000000000000" )], {
+      gas: 43000
+    });
+
+    const theRatio = "200000000000000000000000000000";
+    await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cWbtcAddress, numToHex( theRatio )], {
+      gas: 43000
+    });
+  }
+
+  return {
+    source,
+    anchor,
+    anchorMantissa,
+    priceData,
+    delfi,
+    proxyPriceOracle,
+    ctokens,
+    postPrices,
+    primeAnchor,
+    getPrice
+  };
+}
+
+
+describe('AnchoredPriceView', () => {
+  let source,
+      anchor,
+      anchorMantissa,
+      priceData,
+      delfi,
+      proxyPriceOracle,
+      ctokens,
+      postPrices,
+      primeAnchor,
+      getPrice;
+
+  const timestamp = time() - 5;
+  describe('Anchoring', () => {
+    beforeEach(async done => {
+      ({
+        source,
+        anchor,
+        anchorMantissa,
+        priceData,
+        delfi,
+        proxyPriceOracle,
+        ctokens,
+        postPrices,
+        primeAnchor,
+        getPrice
+      } = await setup());
+      await primeAnchor();
+      done();
+    });
+
+    it('posting 0 anchor price should guard price and not revert', async () => {
+      await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cUsdcAddress, 1], {
+        gas: 43000
+      });
+      const post1 = await postPrices(
+        timestamp,
+        [[['ETH', 91]]],
+        ['ETH'],
+        source
+      );
+      expect(post1.events.PriceGuarded).not.toBe(undefined);
+      expect(post1.events.PricePosted).toBe(undefined);
+      expect(await getPrice('ETH')).numEquals(0);
+    });
+
+    it('posting within anchor should update stored value', async () => {
+      const post1 = await postPrices(
+        timestamp,
+        [[['ETH', 492]]],
+        ['ETH'],
+        source
+      );
+
+      expect(post1.gasUsed).toBeLessThan(253000);
+      expect(post1.events.PriceUpdated.returnValues.symbol).toBe('ETH');
+      expect(post1.events.PriceUpdated.returnValues.price).numEquals(492e6);
+      expect(await getPrice('ETH')).numEquals(492e6);
+
+      // double the dollar ratio
+      await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cUsdcAddress, "4000000000000000000000000000"], {
+        gas: 43000
+      });
+      const post2 = await postPrices(
+        timestamp + 1,
+        [[['ETH', 250]]],
+        ['ETH'],
+        source
+      );
+      expect(post2.gasUsed).toBeLessThan(252000);
+      expect(post2.events.PriceUpdated.returnValues.symbol).toBe('ETH');
+      expect(post2.events.PriceUpdated.returnValues.price).numEquals(250e6);
+      expect(await getPrice('ETH')).numEquals(250e6);
+    });
+
+    it('should not update source price if anchor is much lower', async () => {
+      const post1 = await postPrices(
+        timestamp,
+        [[['ETH', 1000]]],
+        ['ETH'],
+        source
+      );
+      expect(post1.events.PriceUpdated).toBe(undefined);
+      expect(post1.events.PriceGuarded).not.toBe(undefined);
+      expect(await getPrice('ETH')).numEquals(0);
+    });
+
+    it('should not update source price if anchor is much higher', async () => {
+      const post1 = await postPrices(
+        timestamp,
+        [[['ETH', 116]]],
+        ['ETH'],
+        source
+      );
+      expect(post1.events.PriceUpdated).toBe(undefined);
+      expect(post1.events.PriceGuarded).not.toBe(undefined);
+      expect(await getPrice('ETH')).numEquals(0);
+    });
+
+    it('posting all source for two assets should update stored values', async () => {
+      const post1 = await postPrices(
+        timestamp,
+        [
+          [['ETH', 510], ['BTC', 11000]],
+        ],
+        ['ETH', 'BTC'],
+        source
+      );
+      expect(post1.gasUsed).toBeLessThan(650000);
+      expect(post1.events.PriceUpdated[0].returnValues.symbol).toBe('ETH');
+      expect(post1.events.PriceUpdated[0].returnValues.price).numEquals(510e6);
+      expect(await getPrice('ETH')).numEquals(510e6);
+
+      expect(post1.events.PriceUpdated[1].returnValues.symbol).toBe('BTC');
+      expect(post1.events.PriceUpdated[1].returnValues.price).numEquals(11000e6);
+      expect(await getPrice('BTC')).numEquals(11000e6);
+    });
+
+    it('view should use most recent post from source', async () => {
+      await postPrices(
+        timestamp,
+        [
+          [['ETH', 510], ['BTC', 11000]],
+        ],
+        ['ETH', 'BTC'],
+        source
+      );
+      const post2 = await postPrices(
+        timestamp + 1,
+        [
+          [['ETH', 502], ['BTC', 10008]],
+        ],
+        ['ETH', 'BTC'],
+        source
+      );
+
+      expect(post2.events.PriceUpdated[0].returnValues.price).numEquals(502e6);
+      expect(await getPrice('ETH')).numEquals(502e6);
+
+      expect(post2.events.PriceUpdated[1].returnValues.price).numEquals(10008e6);
+      expect(await getPrice('BTC')).numEquals(10008e6);
+    });
+
+    it('should revert on posting invalid message', async () => {
+      await expect(
+        send(delfi, 'postPrices', [['0xabc'], ['0x123'], []], { gas: 5000000 })
+      ).rejects.toRevert();
+    });
+
+    it('posting from non-source should not change median or emit event', async () => {
+      // set some baseline numbers
+      await postPrices(
+        timestamp,
+        [
+          [['ETH', 500]],
+        ],
+        ['ETH'],
+        source
+      );
+
+      let nonSource = web3.eth.accounts.privateKeyToAccount('0x666ee777e72b8c042e05ef41d1db0f17f1fcb0e8150b37cfad6993e4373bdf10');
+
+      const post1 = await postPrices(
+        timestamp + 1,
+        [[['ETH', 595]]],
+        ['ETH'],
+        nonSource
+      );
+      expect(post1.events.PriceGuarded).toBe(undefined);
+      expect(post1.events.PriceUpdated).toBe(undefined);
+      expect(await getPrice('ETH')).numEquals(500e6);
+    });
+  });
+
+  describe("getAnchorPrice", () => {
+    beforeEach(async () => {
+      ({
+        source,
+        priceData,
+        delfi,
+        proxyPriceOracle,
+        ctokens,
+        postPrices
+      } = await setup());
+    });
+
+    it("returns one with 6 decimals when given usd or usdt", async () => {
+
+      let usdcPrice = "5812601720530109000000000000";
+      const converted_usdc_price = await call(delfi,'getAnchorPrice',[ctokens.cUsdcAddress, usdcPrice]);
+      expect(converted_usdc_price).toEqual(1e6.toString());
+
+      const converted_usdt_price = await call(delfi,'getAnchorPrice',[ctokens.cUsdtAddress, usdcPrice]);
+      expect(converted_usdt_price).toEqual(1e6.toString());
+
+    });
+
+    it("converts eth price through proxy usdc, with 6 decimals", async () => {
+      await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cEthAddress, numToHex(1e18)]);
+      // ~ $172 eth
+      let usdcPrice = "5812601720530109000000000000";
+      const converted_eth_price = await call(delfi,'getAnchorPrice',[ctokens.cEthAddress, usdcPrice]);
+      expect(converted_eth_price).toEqual(172.04e6.toString());
+    });
+
+    // [open oracle symbol, proxy price in ether, open oracle price in usd]
+    [
+      ["ETH", 1e18, 172.04e6],
+      ["SAI", 5905879257418508, 1.016047e6],
+      ["DAI", 5905879257418508, 1.016047e6],
+
+      ["BAT", 931592500000000, 0.160271e6],
+      ["REP", 56128970000000000, 9.656427e6],
+      ["ZRX", 985525000000000, 0.169549e6],
+      ["BTC", "399920015996800660000000000000", 6880.223955e6] // 8 decimals underlying -> 10 extra decimals on proxy 
+    ].forEach( ([openOracleKey, proxyPrice, expectedOpenOraclePrice ]) => {
+      it(`converts  ${openOracleKey} price through proxy usdc, with 6 decimals`, async () => {
+        let tokenAddress = await call(delfi, 'getCTokenAddress', [openOracleKey]);
+        await send(proxyPriceOracle, 'setUnderlyingPrice', [tokenAddress, numToHex(proxyPrice)]);
+        // ~ $172 eth
+        let usdcPrice = "5812601720530109000000000000";
+        const converted_price = await call(delfi,'getAnchorPrice',[tokenAddress, usdcPrice]);
+        expect(converted_price).toEqual(expectedOpenOraclePrice.toString());
+      });
+    });
+
+  });
+
+  describe("getUnderlyingPrice", () => {
+    beforeEach(async () => {
+      ({
+        source,
+        priceData,
+        delfi,
+        proxyPriceOracle,
+        ctokens,
+        postPrices
+      } = await setup(1));
+
+      // set usdc price so anchoring can use it
+      let usdcPrice = "5812601720530109000000000000";
+      await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cUsdcAddress, numToHex(usdcPrice)]);
+    });
+
+
+    ["USDC", "USDT"].forEach( (openOracleKey) => {
+      it(`returns 1 with 18 + 12 decimals for ${openOracleKey}`, async () => {
+        let tokenAddress = await call(delfi, 'getCTokenAddress', [openOracleKey]);
+        const underlying_price = await call(delfi,'getUnderlyingPrice',[tokenAddress]);
+
+        expect(underlying_price).toEqual("1000000000000000000000000000000");
+      });
+    });
+
+
+    it("returns source price with 18 + 10 decimals for BTC", async () =>{
+      let tokenAddress = await call(delfi, 'getCTokenAddress', ["BTC"]);
+      await send(proxyPriceOracle, 'setUnderlyingPrice', [tokenAddress, numToHex("399920015996800660000000000000")]);
+      const post1 = await postPrices(
+        time() - 5,
+        [[["BTC", 6880.223955]]],
+        ["BTC"],
+        source
+      );
+
+      const underlying_price = await call(delfi,'getUnderlyingPrice',[tokenAddress]);
+
+      const actualOpenOraclePrice = await call(delfi, 'prices', [ "BTC" ]);
+
+      expect(underlying_price).toEqual( numToBigNum(actualOpenOraclePrice).mul(numToBigNum("10000000000000000000000")).toString(10));
+    });
+
+    [
+      ["ETH", 1e18, 172.04],
+    ].forEach( ([openOracleKey, anchorPrice, openOraclePrice]) => {
+      it(`returns source price with 18 decimals for ${openOracleKey}`, async () => {
+        let tokenAddress = await call(delfi, 'getCTokenAddress', [openOracleKey]);
+        await send(proxyPriceOracle, 'setUnderlyingPrice', [tokenAddress, numToHex(anchorPrice)]);
+        const post1 = await postPrices(
+          time() - 5,
+          [[[openOracleKey, openOraclePrice]]],
+          [openOracleKey],
+          source
+        );
+
+        const underlying_price = await call(delfi,'getUnderlyingPrice',[tokenAddress]);
+
+        const actualOpenOraclePrice = openOraclePrice * 1e6;
+
+        expect(underlying_price).toEqual( numToBigNum(actualOpenOraclePrice).mul(numToBigNum("1000000000000")).toString(10));
+      });
+    });
+
+    [
+      ["SAI", 5905879257418508, 1016047],
+      ["DAI", 5905879257418508, 1016047],
+      ["BAT", 931592500000000, 160271],
+      ["REP", 56128970000000000, 9656427],
+      ["ZRX", 985525000000000, 169549]
+    ].forEach( ([openOracleKey, anchorPrice, openOraclePrice]) => {
+      it(`returns anchor price converted to dollars with 18 decimals for ${openOracleKey} converted through open oracle eth price`, async () => {
+
+        let tokenAddress = await call(delfi, 'getCTokenAddress', [openOracleKey]);
+        await send(proxyPriceOracle, 'setUnderlyingPrice', [tokenAddress, numToHex(anchorPrice)]);
+        await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cEthAddress, numToHex(1e18)]);
+        const post1 = await postPrices(
+          time() - 5,
+          [[['ETH', 172.04]]],
+          ['ETH'],
+          source
+        );
+
+        const underlying_price = await call(delfi,'getUnderlyingPrice',[tokenAddress]);
+
+
+        expect(underlying_price).toEqual(numToBigNum(openOraclePrice).mul(numToBigNum("1000000000000")).toString(10));
+      });
+
+      it("returns proxy price with 18 decimals for SAI, converted through Open Oracle Eth price", async () => {
+        // ["SAI", 5905879257418508, 1.016047],
+        await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cSaiAddress, numToHex(5905879257418508)]);
+        await send(proxyPriceOracle, 'setUnderlyingPrice', [ctokens.cEthAddress, numToHex(1e18)]);
+
+        const post1 = await postPrices(
+          time() - 5,
+          [[['ETH', 172.04]]],
+          ['ETH'],
+          source
+        );
+
+        const underlying_price = await call(delfi,'getUnderlyingPrice',[ctokens.cSaiAddress]);
+
+        expect(underlying_price).toEqual("1016047000000000000");
+      });
+
+    });
+  });
+});
