99ce1f646fdb0ea92478beac8d9ff9c8abd4c8e9
Antonina Norair <antonina.norair@gmail.com>
2020-06-11 15:15:58 -0700

    Add initial draft of uniswap oracles

---
+++ contracts/Uniswap/UniswapLaggingWindowOracle.sol
@@ -0,0 +1,68 @@
+pragma solidity =0.6.6;
+pragma experimental ABIEncoderV2;
+
+import "./UniswapLib.sol";
+
+
+contract UniswapLaggingWindowOracle {
+    using FixedPoint for *;
+
+    struct Observation {
+        uint timestamp;
+        uint price0Cumulative;
+        uint price1Cumulative;
+    }
+
+    // Period for comparing new and old observations
+    uint public constant PERIOD = 30 minutes;
+
+    // mapping from pair address to an old observation
+    mapping(address => Observation) public oldObservations;
+    // mapping from pair address to an new observation
+    mapping(address => Observation) public newObservations;
+
+    event ObservationsUpdated(address indexed pair, uint price0Cumulative, uint price1Cumulative, uint timeElapsed);
+
+    // Get TWAP prices per pair at the current timestamp.
+    // Update new and old observations of lagging window if period elapsed.
+    function poke(address pair) external returns (uint price0Average, uint price1Average) {
+        Observation storage newObservation = oldObservations[pair];
+        Observation storage oldObservation = newObservations[pair];
+
+        (uint price0Cumulative, uint price1Cumulative,) = UniswapV2OracleLibrary.currentCumulativePrices(pair);
+        uint timeElapsed = block.timestamp - newObservation.timestamp;
+
+        // Update new and old observations if elapsed time is bigger or equal to PERIOD
+        if (timeElapsed >= PERIOD) {
+            // Update old observation
+            // oldObservation = newObservation;
+            oldObservation.timestamp = newObservation.timestamp;
+            oldObservation.price0Cumulative = newObservation.price0Cumulative;
+            oldObservation.price1Cumulative = newObservation.price1Cumulative;
+
+            // Update new observation
+            newObservation.timestamp = block.timestamp;
+            newObservation.price0Cumulative = price0Cumulative;
+            newObservation.price1Cumulative = price1Cumulative;
+
+            emit ObservationsUpdated(pair, price0Cumulative, price1Cumulative, timeElapsed);
+        }
+
+        price0Average = computeTWAP(oldObservation.price0Cumulative, price0Cumulative, timeElapsed);
+        price1Average = computeTWAP(oldObservation.price1Cumulative, price1Cumulative, timeElapsed);
+    }
+
+    // given the cumulative prices of the start and end of a period, and the length of the period,
+    // compute the average price
+    function computeTWAP(
+        uint priceCumulativeStart,
+        uint priceCumulativeEnd,
+        uint timeElapsed
+    ) private pure returns (uint amountOut) {
+        // overflow is desired.
+        FixedPoint.uq112x112 memory priceAverage = FixedPoint.uq112x112(
+            uint224((priceCumulativeEnd - priceCumulativeStart) / timeElapsed)
+        );
+        amountOut = priceAverage.mul(1e18).decode144();
+    }
+}
---
+++ contracts/Uniswap/UniswapOracle.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.6.6;
+pragma experimental ABIEncoderV2;
+
+import "./UniswapLib.sol";
+
+/**
+ * @notice Uniswap Oracle based on https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/examples/ExampleOracleSimple.sol.
+ * @dev A simple oracle for storing TWAP for 6 assets pairs - ETH, WBTC, ZRX, BAT, REP, DAI.
+ * @author Compound Labs, Inc.
+ */
+contract UniswapOracle {
+    using FixedPoint for *;
+
+     /// @notice The IUniswapV2Pair Token Pairs
+    struct TokenPairs {
+        address USDC_ETH_pair;
+        address DAI_ETH_pair;
+        address BAT_ETH_pair;
+        address REP_ETH_pair;
+        address WETH_ZRX_pair;
+        address WBTC_ETH_pair;
+    }
+
+    // Addresses of the Token Pairs contracts
+    address public immutable USDC_ETH_pair;
+    address public immutable DAI_ETH_pair;
+    address public immutable BAT_ETH_pair;
+    address public immutable REP_ETH_pair;
+    address public immutable WETH_ZRX_pair;
+    address public immutable WBTC_ETH_pair;
+
+    struct TokenPriceData {
+        address pair;
+        uint price0CumulativeLast;
+        uint price1CumulativeLast;
+        uint32 blockTimestampLast;
+        FixedPoint.uq112x112 price0Average;
+        FixedPoint.uq112x112 price1Average;
+    }
+
+    mapping(string => TokenPriceData) pairPrices;
+
+    uint public immutable period;
+
+    constructor(uint period_, TokenPairs memory pairs) public {
+        period = period_;
+
+        USDC_ETH_pair = pairs.USDC_ETH_pair;
+        DAI_ETH_pair = pairs.DAI_ETH_pair;
+        BAT_ETH_pair = pairs.BAT_ETH_pair;
+        REP_ETH_pair = pairs.REP_ETH_pair;
+        WETH_ZRX_pair = pairs.WETH_ZRX_pair;
+        WBTC_ETH_pair = pairs.WBTC_ETH_pair;
+
+        // Init all supported token pairs
+        initTokenPair("ETH", pairs.USDC_ETH_pair);
+        initTokenPair("DAI", pairs.DAI_ETH_pair);
+        initTokenPair("BAT", pairs.BAT_ETH_pair);
+        initTokenPair("REP", pairs.REP_ETH_pair);
+        initTokenPair("ZRX", pairs.WETH_ZRX_pair);
+        initTokenPair("BTC", pairs.WBTC_ETH_pair);
+    }
+
+    function initTokenPair(string memory symbol, address _pair) internal {
+        IUniswapV2Pair pair = IUniswapV2Pair(_pair);
+
+        uint112 reserve0;
+        uint112 reserve1;
+        uint32 blockTimestampLast;
+        (reserve0, reserve1, blockTimestampLast) = pair.getReserves();
+        require(reserve0 != 0 && reserve1 != 0, "UniswapOracle: NO_RESERVES"); // ensure that there's liquidity in the pair
+
+        pairPrices[symbol].pair = address(pair);
+        pairPrices[symbol].price0CumulativeLast = pair.price0CumulativeLast(); // fetch the current accumulated price value (1 / 0)
+        pairPrices[symbol].price1CumulativeLast = pair.price1CumulativeLast();  // fetch the current accumulated price value (0 / 1)
+        pairPrices[symbol].blockTimestampLast = blockTimestampLast;
+    }
+
+    function update(string calldata symbol) external {
+        (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) =
+            UniswapV2OracleLibrary.currentCumulativePrices(pairPrices[symbol].pair);
+        uint32 timeElapsed = blockTimestamp - pairPrices[symbol].blockTimestampLast;
+
+        // ensure that at least one full period has passed since the last update
+        require(timeElapsed >= period, "UniswapOracle: PERIOD_NOT_ELAPSED");
+
+        // overflow is desired, casting never truncates
+        // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+        pairPrices[symbol].price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - pairPrices[symbol].price0CumulativeLast) / timeElapsed));
+        pairPrices[symbol].price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - pairPrices[symbol].price1CumulativeLast) / timeElapsed));
+
+        pairPrices[symbol].price0CumulativeLast = price0Cumulative;
+        pairPrices[symbol].price1CumulativeLast = price1Cumulative;
+        pairPrices[symbol].blockTimestampLast = blockTimestamp;
+    }
+
+    // note this will always return 0 before update has been called successfully for the first time.
+    function getPrice(string calldata symbol) external view returns (uint price0Average, uint price1Average) {
+        return (pairPrices[symbol].price0Average.mul(1e18).decode144(), pairPrices[symbol].price1Average.mul(1e18).decode144());
+    }
+
+}
