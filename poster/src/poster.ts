09493d134f24fffedac77f07b8a628012bcab662
Antonina Norair <antonina.norair@gmail.com>
2020-06-26 13:30:02 -0700

    Fix and error with posting, plus formatting

--- poster/src/poster.ts
+++ poster/src/poster.ts
@@ -6,47 +6,16 @@ import AbiCoder from 'web3-eth-abi';
 import { getDataAddress, getPreviousPrice, getSourceAddress } from './prev_price';
 import { BigNumber as BN } from 'bignumber.js';
 
-async function main(sources : string,
-                    senderKey : string,
-                    viewAddress : string,
-                    functionSig : string,
-                    gas: number,
-                    delta: number,
-                    assets: string,
-                    web3 : Web3) {
+async function main(sources: string,
+  senderKey: string,
+  viewAddress: string,
+  functionSig: string,
+  gas: number,
+  delta: number,
+  assets: string,
+  web3: Web3) {
   const payloads = await fetchPayloads(sources.split(","));
-  const dataAddress = await getDataAddress(viewAddress, web3);
-  const supportedAssets = assets.split(",");
-
-  await Promise.all(payloads.map(async payload => {
-    const sourceAddress = await getSourceAddress(dataAddress, payload.messages[0], payload.signatures[0], web3);
-    
-    const filteredPrices = {};
-    const filteredMessages: string[] = [];
-    const filteredSignatures: string[] = [];
-    let index = 0;
-    for (const [asset, price] of Object.entries(payload.prices)) {
-      // Post only prices for supported assets
-      if (!supportedAssets.includes(asset)) continue;
-      const prev_price = await getPreviousPrice(sourceAddress, asset, dataAddress, web3);
-      console.log(`For asset ${asset}: prev price = ${prev_price}, new price = ${price}`);
-
-      // Update price if new price is different by more than delta % from previous price
-      // Update all asset prices if only 1 asset price is different
-      if (!inDeltaRange(delta, Number(price), prev_price)) {
-        filteredPrices[asset] = price;
-        filteredMessages.push(payload.messages[index]);
-        filteredSignatures.push(payload.signatures[index]);
-      }
-      index++;
-    }
-    payload.prices = filteredPrices;
-    payload.messages = filteredMessages;
-    payload.signatures = filteredSignatures;
-  }))
-
-  // Filter payloads with no prices for an update
-  const filteredPayloads = payloads.filter(payload => payload.messages.length > 0);
+  const filteredPayloads = await filterPayloads(payloads, viewAddress, assets, delta, web3);
   if (filteredPayloads.length != 0) {
     const gasPrice = await fetchGasPrice();
     const trxData = buildTrxData(filteredPayloads, functionSig);
@@ -62,21 +31,60 @@ async function main(sources : string,
   }
 }
 
-// If new price is less that delta percent different form the old price, do not post new price
-function inDeltaRange(delta:number, price: number, prev_price: number) {
-  // Always update prices if delta is set to 0 or delta is not within expected range [0..100]%
-   if (delta <= 0 || delta > 100) return false;
+async function filterPayloads(payloads: DelFiReporterPayload[],
+  viewAddress: string,
+  assets: string,
+  delta: number,
+  web3: Web3): Promise<DelFiReporterPayload[]> {
+  const dataAddress = await getDataAddress(viewAddress, web3);
+  const supportedAssets = assets.split(",");
 
-   const minDifference = new BN(prev_price).multipliedBy(delta).dividedBy(100);
-   const difference = new BN(prev_price).minus(new BN(price).multipliedBy(1e6)).abs();
-   return difference.isLessThanOrEqualTo(minDifference);
+  await Promise.all(payloads.map(async payload => {
+    const sourceAddress = await getSourceAddress(dataAddress, payload.messages[0], payload.signatures[0], web3);
+
+    const filteredPrices = {};
+    const filteredMessages: string[] = [];
+    const filteredSignatures: string[] = [];
+    let index = 0;
+    for (const [asset, price] of Object.entries(payload.prices)) {
+      // Post only prices for supported assets, skip prices for unregistered assets
+      if (!supportedAssets.includes(asset)) {index++; continue;}
+      const prev_price = await getPreviousPrice(sourceAddress, asset, dataAddress, web3);
+      console.log(`For asset ${asset}: prev price = ${prev_price}, new price = ${price}`);
+
+      // Update price only if new price is different by more than delta % from the previous price
+      if (!inDeltaRange(delta, Number(price), prev_price)) {
+        console.log(`Not in delta range for asset ${asset}, price = ${Number(price)}, prev price = ${prev_price}`);
+        filteredPrices[asset] = price;
+        filteredMessages.push(payload.messages[index]);
+        filteredSignatures.push(payload.signatures[index]);
+      }
+      index++;
+    }
+    payload.prices = filteredPrices;
+    payload.messages = filteredMessages;
+    payload.signatures = filteredSignatures;
+  }))
+
+  // Filter payloads with no updated prices
+  const filteredPayloads = payloads.filter(payload => payload.messages.length > 0);
+  return filteredPayloads;
 }
 
-async function fetchPayloads(sources : string[], fetchFn=fetch) : Promise<DelFiReporterPayload[]> {
+// Checks if new price is less than delta percent different form the old price
+function inDeltaRange(delta: number, price: number, prev_price: number) {
+  // Always update prices if delta is set to 0 or delta is not within expected range [0..100]%
+  if (delta <= 0 || delta > 100) return false;
+  const minDifference = new BN(prev_price).multipliedBy(delta).dividedBy(100);
+  const difference = new BN(prev_price).minus(new BN(price).multipliedBy(1e6)).abs();
+  return difference.isLessThanOrEqualTo(minDifference);
+}
+
+async function fetchPayloads(sources: string[], fetchFn = fetch): Promise<DelFiReporterPayload[]> {
   let sourcePromises = sources.map(async (source) => {
     try {
       let response;
-      if(source == "https://api.pro.coinbase.com/oracle") {
+      if (source == "https://api.pro.coinbase.com/oracle") {
         const crypto = require('crypto');
 
         const key_id = <string>process.env.API_KEY_ID;
@@ -92,7 +100,7 @@ async function fetchPayloads(sources : string[], fetchFn=fetch) : Promise<DelFiR
         let what = timestamp + method + "/oracle";
 
         // decode the base64 secret
-        let key =  Buffer.from(secret, 'base64');
+        let key = Buffer.from(secret, 'base64');
 
         // create a sha256 hmac with the secret
         let hmac = crypto.createHmac('sha256', key);
@@ -125,7 +133,7 @@ async function fetchPayloads(sources : string[], fetchFn=fetch) : Promise<DelFiR
   return (await Promise.all(sourcePromises)).filter(x => x != null);
 }
 
-async function fetchGasPrice(fetchFn=fetch) : Promise<number> {
+async function fetchGasPrice(fetchFn = fetch): Promise<number> {
   try {
     let source = "https://api.compound.finance/api/gas_prices/get_gas_price";
     let response = await fetchFn(source);
@@ -139,7 +147,7 @@ async function fetchGasPrice(fetchFn=fetch) : Promise<number> {
   }
 }
 
-function buildTrxData(payloads : DelFiReporterPayload[], functionSig : string) : string {
+function buildTrxData(payloads: DelFiReporterPayload[], functionSig: string): string {
   const types = findTypes(functionSig);
 
   console.log("payloads = ", payloads);
@@ -151,13 +159,13 @@ function buildTrxData(payloads : DelFiReporterPayload[], functionSig : string) :
 
   // see https://github.com/ethereum/web3.js/blob/2.x/packages/web3-eth-abi/src/AbiCoder.js#L112
   return (<any>AbiCoder).encodeFunctionSignature(functionSig) +
-         (<any>AbiCoder)
-           .encodeParameters(types, [messages, signatures, [...upperCaseDeDuped]])
-           .replace('0x', '');
+    (<any>AbiCoder)
+      .encodeParameters(types, [messages, signatures, [...upperCaseDeDuped]])
+      .replace('0x', '');
 }
 
 // e.g. findTypes("postPrices(bytes[],bytes[],string[])")-> ["bytes[]","bytes[]","string[]"]
-function findTypes(functionSig : string) : string[] {
+function findTypes(functionSig: string): string[] {
   // this unexported function from ethereumjs-abi is copy pasted from source
   // see https://github.com/ethereumjs/ethereumjs-abi/blob/master/lib/index.js#L81
   let parseSignature = function (sig) {
@@ -193,7 +201,7 @@ function findTypes(functionSig : string) : string[] {
   return parseSignature(functionSig).args;
 }
 
-function getEnvVar(name : string): string {
+function getEnvVar(name: string): string {
   const result: string | undefined = process.env[name];
 
   if (result) {
