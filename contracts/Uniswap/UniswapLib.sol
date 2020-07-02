959550c4862965a4cb1563d238c6efa1502ca985
Antonina Norair <antonina.norair@gmail.com>
2020-07-02 09:40:57 -0700

    Clean up UniswapLib, delete unused SafeMath lib

--- contracts/Uniswap/UniswapLib.sol
+++ contracts/Uniswap/UniswapLib.sol
@@ -37,21 +37,6 @@ library FixedPoint {
     }
 }
 
-// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
-library SafeMath {
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x, 'ds-math-add-overflow');
-    }
-
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, 'ds-math-sub-underflow');
-    }
-
-    function mul(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
-    }
-}
-
 // library with helper methods for oracles that are concerned with computing average prices
 library UniswapV2OracleLibrary {
     using FixedPoint for *;
