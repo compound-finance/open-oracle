d04faa3ed71269a3010d378a70554072f3de5e69
Geoffrey Hayes <hayesgm@gmail.com>
2020-07-14 19:08:36 -0700

    Open Price Feed Poster Clean-up
    This patch is simply cleaning up some of the Open Price Feed code. I started and I couldn't stop, so it's a nice overhaul just trying to cleanup the functions and interface without significantly changing the behavior. There are some minor CLI differences that we'll need to account for. This is going to be the basis for the branch to handle updating Uniswap for testnets. We do change the CLI and some minor features, but I think this is coupled with some pretty serious bugfixes, as well.

--- poster/src/prev_price.ts
+++ poster/src/prev_price.ts
@@ -1,55 +1,32 @@
 import Web3 from 'web3';
-import { TransactionConfig } from 'web3-core';
-import AbiCoder from 'web3-eth-abi';
-import { findTypes } from './poster';
+import { read } from './util';
 
-async function getPreviousPrice(sourceAddress: string, asset: string, dataAddress: string, web3 : Web3) {
-    const functionSig = 'getPrice(address,string)';
-    const types = findTypes(functionSig);
-  
-    const callData = (<any>AbiCoder).encodeFunctionSignature(functionSig) +
-           (<any>AbiCoder)
-             .encodeParameters(types, [sourceAddress, asset.toUpperCase()])
-             .replace('0x', '');
-    const call = <TransactionConfig> {
-      data: callData,
-      //Price open oracle data
-      to: dataAddress
-    }
-    const price = web3.utils.hexToNumber(await web3.eth.call(call));
-    console.log(`Previous price of ${asset.toUpperCase()} for source ${sourceAddress} = ${price}`);
-    return price;
-  }
-  
-  async function getDataAddress(viewAddress: string, web3: Web3) {
-    const callData = (<any>AbiCoder).encodeFunctionSignature('data()') 
-    const call = <TransactionConfig> {
-      data: callData,
-      to: viewAddress
-    }
-    return web3.eth.abi.decodeParameter('address', await web3.eth.call(call)).toString();
-  }
-  
-  async function getSourceAddress(dataAddress: string, message: string, signature: string, web3 : Web3) {
-    const functionSig = 'source(bytes,bytes)';
-    const types = findTypes(functionSig);
-  
-    const callData = (<any>AbiCoder).encodeFunctionSignature(functionSig) +
-           (<any>AbiCoder)
-             .encodeParameters(types, [message, signature])
-             .replace('0x', '');
-    const call = <TransactionConfig> {
-      data: callData,
-      //Price open oracle data
-      to: dataAddress
-    }
-    return web3.eth.abi.decodeParameter('address', await web3.eth.call(call)).toString();
-  }
+export async function getPreviousPrice(sourceAddress: string, asset: string, dataAddress: string, web3: Web3) {
+  return await read(
+    dataAddress,
+    'getPrice(address,string)',
+    [sourceAddress, asset.toUpperCase()],
+    'uint256',
+    web3
+  );
+}
 
-  const exportFunctions = {
-    getPreviousPrice, 
-    getSourceAddress, 
-    getDataAddress
-  };
-  export default exportFunctions;
-  
+export async function getDataAddress(viewAddress: string, web3: Web3) {
+  return await read(
+    viewAddress,
+    'priceData()',
+    [],
+    'address',
+    web3
+  );
+}
+
+export async function getSourceAddress(dataAddress: string, message: string, signature: string, web3 : Web3) {
+  return await read(
+    dataAddress,
+    'source(bytes,bytes)',
+    [message, signature],
+    'address',
+    web3
+  );
+}
