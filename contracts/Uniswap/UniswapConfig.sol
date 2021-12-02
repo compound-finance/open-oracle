86d61d313967e9f62e4d5d275cfb457fba6e4230
Kevin Tjiam <kevin@kevincharm.com>
2021-12-02 17:08:16 +0100

    A8: pin pragma to solc 0.8.7

--- contracts/Uniswap/UniswapConfig.sol
+++ contracts/Uniswap/UniswapConfig.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.7;
+pragma solidity =0.8.7;
 
 interface CErc20 {
     function underlying() external view returns (address);
--- contracts/Uniswap/UniswapLib.sol
+++ contracts/Uniswap/UniswapLib.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.7;
+pragma solidity =0.8.7;
 
 // From: https://github.com/Uniswap/uniswap-v3-core
 
