6b745544d2a7959f458d217c5522e6ef192c74aa
Kevin Tjiam <kevin@kevincharm.com>
2021-12-02 14:45:06 +0100

    A1: remove unused struct Slot0 code

--- contracts/Uniswap/UniswapLib.sol
+++ contracts/Uniswap/UniswapLib.sol
@@ -199,24 +199,6 @@ library TickMath {
     }
 }
 
-struct Slot0 {
-    // the current price
-    uint160 sqrtPriceX96;
-    // the current tick
-    int24 tick;
-    // the most-recently updated index of the observations array
-    uint16 observationIndex;
-    // the current maximum number of observations that are being stored
-    uint16 observationCardinality;
-    // the next maximum number of observations to store, triggered in observations.write
-    uint16 observationCardinalityNext;
-    // the current protocol fee as a percentage of the swap fee taken on withdrawal
-    // represented as an integer denominator (1/x)%
-    uint8 feeProtocol;
-    // whether the pool is locked
-    bool unlocked;
-}
-
 interface IUniswapV3Pool {
     /// @notice The first of the two tokens of the pool, sorted by address
     /// @return The token contract address
