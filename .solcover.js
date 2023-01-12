d1ba268342d6264817fb4138ce4b18cab69a9d2f
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2023-01-12 18:00:43 +0800

    add tests for UniswapConfig

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
   skipFiles: [
     "test/MockChainlinkOCRAggregator.sol",
     "test/UniswapV3SwapHelper.sol",
-    "test/UniswapLib.sol",
+    "Uniswap/UniswapLib.sol",
   ],
   mocha: {
     grep: "@skip-on-coverage", // skip everything with this tag
